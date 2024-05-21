8290f20d7d58a64336139e70cab4d2e5f74e2176
willmo3 <thelordofbouge@gmail.com>
2024-05-21 07:42:01 -0400

    add tierconfig

---
+++ src/main/net/williserver/tiers/TierConfig.kt
@@ -0,0 +1,60 @@
+package net.williserver.tiers
+
+import org.bukkit.configuration.file.FileConfiguration
+
+/**
+ * Responsible for parsing config options and restoring defaults, if need be.
+ *
+ * @param handler Logger
+ * @param config Configuration file to parse options from.
+ * @author Willmo3
+ */
+class TierConfig(private val handler: LogHandler, private val config: FileConfiguration) {
+    // ***** CONFIG FIELDS ***** //
+    private val tierIntervalOption = "tierInterval"
+    private val tierSizeOption = "tierSize"
+    private val trackNameOption= "trackName"
+    private val useRanksOption = "tierRanks"
+
+    // Config defaults
+    private val defaultTierInterval = 10
+    private val defaultTierSize = 500
+    private val defaultTrackName = "tiers"
+
+    // Loaded fields
+    val tierInterval: Int
+    val tierSize: Int
+    val trackName: String
+    val useRanks: Boolean
+
+    init {
+        // Load config file.
+        val loadedInterval = config.getInt(tierIntervalOption)
+        tierInterval = if (loadedInterval >= 1) {
+            loadedInterval
+        } else {
+            handler.err("Tier interval $loadedInterval too small, must be at least one.")
+            defaultTierInterval
+        }
+
+        val loadedSize = config.getInt(tierSizeOption)
+        tierSize = if (loadedSize >= 1) {
+            loadedSize
+        } else {
+            handler.err("loaded tier size $loadedSize too small, must be at least one.")
+            defaultTierSize
+        }
+
+        // TrackName not optional!
+        val loadedTrackName = config.getString(trackNameOption)
+        trackName = if (loadedTrackName != null) {
+            loadedTrackName
+        } else {
+            handler.err("Track name not specified!")
+            defaultTrackName
+        }
+
+        // This will default to false.
+        useRanks = config.getBoolean(useRanksOption)
+    }
+}
--- src/main/net/williserver/tiers/TiersPlugin.kt
+++ src/main/net/williserver/tiers/TiersPlugin.kt
@@ -1,6 +1,5 @@
 package net.williserver.tiers
 
-import org.apache.commons.lang3.ObjectUtils.Null
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -17,66 +16,21 @@ class TiersPlugin : JavaPlugin() {
     // Name of LuckPerms plugin
     private val luckName = "LuckPerms"
 
-    // ***** CONFIG FIELDS ***** //
-    private val tierIntervalOption = "tierInterval"
-    private val tierSizeOption = "tierSize"
-    private val trackNameOption= "trackName"
-    private val useRanksOption = "tierRanks"
-
-    // Config defaults
-    private val defaultTierInterval = 10
-    private val defaultTierSize = 500
-    private val defaultTrackName = "tiers"
-
     override fun onEnable() {
-        // ***** LOAD CONFIG ***** //
-
-        // Save config with defaults if not present.
+        // Load configuration options
         saveDefaultConfig()
-
-        // Load config file.
-        val loadedInterval = config.getInt(tierIntervalOption)
-        val tierInterval = if (loadedInterval >= 1) {
-            loadedInterval
-        } else {
-            handler.err("Tier interval $loadedInterval too small, must be at least one.")
-            defaultTierInterval
-        }
-
-        val loadedSize = config.getInt(tierSizeOption)
-        val tierSize = if (loadedSize >= 1) {
-            loadedSize
-        } else {
-            handler.err("loaded tier size $loadedSize too small, must be at least one.")
-            defaultTierSize
-        }
-
-        // TrackName not optional!
-        val loadedTrackName = config.getString(trackNameOption)
-        val trackName = if (loadedTrackName != null) {
-            loadedTrackName
-        } else {
-            handler.err("Track name not specified!")
-            defaultTrackName
-        }
-
-        // This will default to false.
-        val useRanks = config.getBoolean(useRanksOption)
-
-        // ***** PREPARE MODEL ***** //
+        val config = TierConfig(handler, config)
 
         // Generate data model.
-        model = TierModel(handler, tierInterval, tierSize, path)
+        model = TierModel(handler, config.tierInterval, config.tierSize, path)
         // Set border width based on starting player count.
         setBorderWidth(model.borderWidth())
 
-        // ***** LUCKPERMS INTEGRATION ***** //
-        val useLuck = useRanks && server.pluginManager.getPlugin(luckName) != null
-
-        // ***** PREPARE LISTENER ***** //
+        // Luckperms Integration
+        val useLuck = config.useRanks && server.pluginManager.getPlugin(luckName) != null
 
         // Add player join listener.
-        val joiner = JoinListener(handler, useLuck, model, trackName)
+        val joiner = JoinListener(handler, useLuck, model, config.trackName)
         server.pluginManager.registerEvents(joiner, this)
         handler.info("Register tiers join listener.")
 
