42e980bae944d3c76ffbf45b49465feca0b5805f
willmo3 <thelordofbouge@gmail.com>
2024-05-25 15:26:57 -0400

    Stub out unit tests, prepare project structure. Switch to java17 -- works better w/ junit

---
+++ src/test/net/williserver/tiers/model/TiersModelTest.kt
@@ -0,0 +1,14 @@
+package net.williserver.tiers.model
+
+import org.junit.jupiter.api.Test
+
+class TiersModelTest {
+
+    @Test
+    fun incrementTier() {
+    }
+
+    @Test
+    fun decrementTier() {
+    }
+}
